4374817f7003c9c458a213880d0028c1ddfd70f2
C. M. de Picciotto <d3p1@d3p1.dev>
2026-07-28 22:08:23 -0300

    feat: add resource feature [#1]

--- src/mcp_server.py
+++ src/mcp_server.py
@@ -38,8 +38,22 @@ def edit_document(
         raise ValueError(f"The document ID {doc_id} does not exist.")
     docs[doc_id] = docs[doc_id].replace(old_string, new_string)
 
-# TODO: Write a resource to return all doc id's
-# TODO: Write a resource to return the contents of a particular doc
+@mcp.resource(
+    "docs://documents",
+    mime_type="application/json"
+)
+def list_documents() -> list[str]:
+    return list(docs.keys())
+
+@mcp.resource(
+    "docs://documents/{doc_id}",
+    mime_type="text/plain"
+)
+def fetch_document(doc_id: str) -> str:
+    if doc_id not in docs:
+        raise ValueError(f"The document ID {doc_id} does not exist.")
+    return docs[doc_id]
+
 # TODO: Write a prompt to rewrite a doc in markdown format
 # TODO: Write a prompt to summarize a doc
 
